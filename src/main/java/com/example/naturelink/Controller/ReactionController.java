f4d07437dedf0cceadbc592fc0d39edd7a0d7931
hajer <hamrouni.hajer@esprit.tn>
2025-04-16 18:11:24 +0100

    anyway

--- src/main/java/com/example/naturelink/Controller/ReactionController.java
+++ src/main/java/com/example/naturelink/Controller/ReactionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/api/reactions")
